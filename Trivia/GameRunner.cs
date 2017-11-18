ddf696185778b508dc0285ea9d5ec83e01b56ec0
Leon de Pruyssenaere de la Woestijne <leonpw@outlook.com>
2017-11-18 13:55:41 +0200

    Add project files.

---
+++ Trivia/GameRunner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using UglyTrivia;
+
+namespace Trivia
+{
+    public class GameRunner
+    {
+        private static bool notAWinner;
+
+        public static void Main(String[] args)
+        {
+            Game aGame = new Game(new[] {"Chet", "Pat", "Sue" });
+
+            int.TryParse(args[0], out int seed);
+
+            Random rand = new Random(seed);
+
+            do
+            {
+                aGame.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+            } while (notAWinner);
+        }
+    }
+}
---
+++ TriviaTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Trivia;
+
+namespace TriviaTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Main_Should()
+        {
+            int seed = 1;
+            RunMainWithSeed(seed);
+        }
+
+        [TestMethod] 
+        public void Main_Should_HaveSameOutputWith10Seeds()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                RunMainWithSeed(i);
+            }
+        }
+
+        private static void RunMainWithSeed(int seed)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+
+            Console.SetOut(sw);
+            GameRunner.Main(new string[] { $"{seed}" });
+
+            string output = sb.ToString();
+
+            Assert.IsNotNull(output);
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"output-{seed}.txt");
+            Trace.WriteLine("The path: " + path);
+            
+            if (File.Exists(path))
+            {
+                string expectedResult = File.ReadAllText(path);
+                Assert.IsTrue(String.Compare(expectedResult, output) == 0);
+            }
+            else
+            {
+                File.WriteAllText(path, output);
+            }
+        }
+    }
+}
